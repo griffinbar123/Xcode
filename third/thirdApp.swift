09ad716e68df72feecfbf4290cf34fada329da5f
Griffin Barnard <griffin.barnard@icloud.com>
2022-11-21 18:57:41 -0600

    Initial Commit

---
+++ third/thirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  thirdApp.swift
+//  third
+//
+//  Created by Griffin Barnard on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct thirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
